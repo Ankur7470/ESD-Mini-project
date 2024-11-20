f319e96ed1a38b7ad76731414ae53dbcc438ae3b
Ankur Sinha <ankur.sinha2019@gmail.com>
2024-11-20 15:58:33 +0530

    Initialize Back-end Part
    Initialized Spring Boot API part, added required dependencies

---
+++ backend/src/test/java/com/ankursinha/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankursinha.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
