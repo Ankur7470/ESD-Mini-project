db07f00f523201fd61de555cb60f591ab82411a4
Ankur Sinha <ankur.sinha2019@gmail.com>
2024-11-22 14:07:39 +0530

    Add StudentController with register and login endpoint

---
+++ backend/src/main/java/com/ankursinha/backend/controller/StudentController.java
@@ -0,0 +1,28 @@
+package com.ankursinha.backend.controller;
+
+import com.ankursinha.backend.dto.LoginRequest;
+import com.ankursinha.backend.entity.Student;
+import com.ankursinha.backend.service.StudentService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping("/register")
+    public void register(@RequestBody Student user) {
+        studentService.register(user);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody @Valid LoginRequest request) {
+        String response = studentService.login(request);
+        return ResponseEntity.ok(response);
+    }
+
+}
