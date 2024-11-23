f0ad2877378737e75dcc131c54789e2fdcf0b53f
Ankur Sinha <ankur.sinha2019@gmail.com>
2024-11-23 15:47:47 +0530

    Add endpoint for student details by Email

--- backend/src/main/java/com/ankursinha/backend/controller/StudentController.java
+++ backend/src/main/java/com/ankursinha/backend/controller/StudentController.java
@@ -1,15 +1,14 @@
 package com.ankursinha.backend.controller;
 
 import com.ankursinha.backend.dto.LoginRequest;
-import com.ankursinha.backend.entity.Placement;
+import com.ankursinha.backend.dto.LoginResponse;
+import com.ankursinha.backend.dto.StudentDetailsResponse;
 import com.ankursinha.backend.entity.Student;
 import com.ankursinha.backend.service.StudentService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 public class StudentController {
@@ -23,15 +22,15 @@ public class StudentController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody @Valid LoginRequest request) {
-        String response = studentService.login(request);
+    public ResponseEntity<LoginResponse> login(@RequestBody @Valid LoginRequest request) {
+        LoginResponse response = studentService.login(request);
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/{studentId}/eligible-placements")
-    public ResponseEntity<List<Placement>> getEligiblePlacements(@PathVariable Long studentId) {
-        List<Placement> eligiblePlacements = studentService.getEligiblePlacements(studentId);
-        return ResponseEntity.ok(eligiblePlacements);
+    @GetMapping("/details/{email}")
+    public ResponseEntity<StudentDetailsResponse> getStudentDetails(@PathVariable String email) {
+        StudentDetailsResponse res = studentService.getStudentDetails(email);
+        return ResponseEntity.ok(res);
     }
 
 }
