01c09bfe1c82be522e7a90ff2411a59e5bc1034d
Ankur Sinha <ankur.sinha2019@gmail.com>
2024-11-28 10:40:22 +0530

    Fix Bug

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -1,18 +1,5 @@
 import PropTypes from 'prop-types';
 
-// const Loading = ({ message }) => {
-//     return (
-//       <div className="flex items-center justify-center min-h-screen bg-gray-50">
-//         <div className="text-center">
-//           <p className="text-gray-600 text-xl font-medium mb-4">
-//             Loading {message}...
-//           </p>
-//           <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-blue-600 border-opacity-50"></div>
-//         </div>
-//       </div>
-//     );
-//   };
-
 const Loading = ({ message }) => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-50">
